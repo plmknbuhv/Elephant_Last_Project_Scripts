25ad8d19148e4d64d04ab3a658e1b832d08bb865
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2026-04-23 00:28:40 +0900

    Change CapsuleColliderDataSO to inherit from AbstractColliderDataSO

--- Enemies/Core/Colliders/Data/CapsuleColliderDataSO.cs
+++ Enemies/Core/Colliders/Data/CapsuleColliderDataSO.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 namespace Code.Enemies.Core.Colliders.Data
 {
     [CreateAssetMenu(fileName = "CapsuleDataSO", menuName = "SO/Enemy/Collider/CapsuleData", order = 0)]
-    public class CapsuleColliderDataSO : SphereColliderDataSO
+    public class CapsuleColliderDataSO : AbstractColliderDataSO
     {
         public float height;
+        public float centerPosY;
+        public float radius;
     }
 }
